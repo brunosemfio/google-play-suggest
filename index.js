3f3db41d83efe1d54ece38c4e03b75093f3d1cf9
brunosemfio <github@brunosemfio.com.br>
2019-05-06 20:15:48 -0300

    versão inicial

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const gplay = require('google-play-scraper')
+
+const readline = require('readline-sync')
+
+async function start() {
+
+    const alphabet = [...'abcdefghijklmnopqrstuvwxyz']
+
+    const startTerm = await askAndReturnQueryTerm()
+
+    const searchTerms = alphabet.map(async i => await querySuggest(`${startTerm} ${i}`))
+
+    const result = await Promise.all(searchTerms)
+
+    console.log('---')
+
+    result.forEach(r => {
+        r.forEach(t => console.log(t))
+        console.log('---')
+    })
+
+    async function askAndReturnQueryTerm() {
+        return readline.question('Search term: ')
+    }
+
+    async function querySuggest(searchTerm) {
+        return gplay.suggest({
+            term: searchTerm,
+            country: 'br',
+            lang: 'pt',
+        })
+    }
+}
+
+start()
